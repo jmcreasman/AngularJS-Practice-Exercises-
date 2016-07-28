9502133dfe3d862448cd636d668572d61b821824
Jon <jmcreasman89@gmail.com>
2016-07-28 15:31:10 -0400

    saving progress

--- SimpleAngularWebApp/src/js/app.js
+++ SimpleAngularWebApp/src/js/app.js
@@ -1,5 +1,5 @@
-var app = angular.module('app, []');
+var app = angular.module('app', []);
 
 app.controller('main', function ($scope) {
-  $scope.message = "Hellow World";
+  $scope.message = "Hello World";
 });
