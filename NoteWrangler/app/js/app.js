0585916a634ae0c28e58a12b1954a75c8ed90f43
Jon <jmcreasman89@gmail.com>
2016-08-11 16:15:48 -0400

    default route setup

--- NoteWrangler/app/js/app.js
+++ NoteWrangler/app/js/app.js
@@ -1,6 +1,19 @@
 angular.module('NoteWrangler', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/notes', {
-    templateUrl: 'templates/pages/notes/index.html'
-  });
+  $routeProvider
+    .when('/', {
+      redirectTo: '/users'
+    })
+    .when('/notes', {
+      templateUrl: 'templates/pages/notes/index.html'
+    })
+    .when('/users', {
+      templateUrl: 'templates/pages/users/index.html'
+    })
+    .when('/notes/new', {
+      templateUrl: 'templates/pages/notes/edit.html'
+    })
+    .otherwise({
+      redirectTo: '/notes'
+    });
 }]);
