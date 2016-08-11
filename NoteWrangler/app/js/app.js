8b990b6d0234f2cab1e7917ae0c9ca572bb3c186
Jon <jmcreasman89@gmail.com>
2016-08-11 15:37:46 -0400

    The Beginning of All Routes

--- NoteWrangler/app/js/app.js
+++ NoteWrangler/app/js/app.js
@@ -1,3 +1,6 @@
-(function(){
-
-})();
+angular.module('NoteWrangler', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/notes', {
+    templateUrl: 'templates/pages/notes/index.html'
+  });
+}]);
