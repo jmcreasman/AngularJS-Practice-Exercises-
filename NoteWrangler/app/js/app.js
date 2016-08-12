81699888eb6e48cd254e7bf21672f966e7f19ebb
Jon <jmcreasman89@gmail.com>
2016-08-12 10:53:15 -0400

    Inside Route Controller

--- NoteWrangler/app/js/app.js
+++ NoteWrangler/app/js/app.js
@@ -5,7 +5,14 @@ angular.module('NoteWrangler', ['ngRoute'])
       redirectTo: '/users'
     })
     .when('/notes', {
-      templateUrl: 'templates/pages/notes/index.html'
+      templateUrl: 'templates/pages/notes/index.html',
+      controller: function($http) {
+        var controller = this;
+        $http({method: 'GET', url: '/notes'}).success(function(data){
+          controller.notes = data;
+        });
+      },
+      controllerAs: 'notesCtrl'
     })
     .when('/users', {
       templateUrl: 'templates/pages/users/index.html'
